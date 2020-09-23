8af25069d1fb6b397e3b515b20c922342ee920f8
Vithop <vithuransoccer1@gmail.com>
2020-09-23 13:08:32 -0400

    save changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,34 +18,59 @@ public class PlayerController : MonoBehaviour {
     public Boundary boundary;
     public GameObject shot;
     public Transform shotSpawn;
+    public SimpleTouchPad touchPad;
 
     private Rigidbody rb_pc;
     private AudioSource a_w;
     private float nextFire;
+    private Quaternion calibrationQuaternion;
 
-
-    private void Start()
+    void Start()
     {
         rb_pc = GetComponent<Rigidbody>();
         a_w = GetComponent<AudioSource>();
+        CalibrateAccellerometer();
     }
 
-    private void Update()
+    void Update()
     {
         if(Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-            a_w.Play();
+            a_w.Play(); 
         }
     }
 
-    private void FixedUpdate()
+    void CalibrateAccellerometer()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    Vector3 FixAcceleration (Vector3 acceleration)
+    {
+        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+        return fixedAcceleration;
+    }
+    void FixedUpdate()
+    {
+        //Mouse Controls
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //Mobile Tilt Controls
+        //Vector3 accelerationRaw = Input.acceleration;
+        //Vector3 acceleration = FixAcceleration(accelerationRaw);
+        //Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+
+        //Mobile TouchPad Controls
+        Vector2 direction = touchPad.GetDirection();
+        Vector3 movement = new Vector3(direction.x, 0.0f, direction.y);
+
         rb_pc.velocity = movement * speed;
 
         rb_pc.position = new Vector3
@@ -57,4 +82,5 @@ public class PlayerController : MonoBehaviour {
 
         rb_pc.rotation = Quaternion.Euler(0.0f, 0.0f, rb_pc.velocity.x * -tilt);
     }
+
 }
---
+++ Assets/Scripts/SimpleTouchPad.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class SimpleTouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
+
+    public float smoothing;
+
+    
+    private bool touched;
+    private int pointerID;
+    private Vector2 smoothDirection;
+    private Vector2 origin;
+    private Vector2 direction;
+
+    public void Awake()
+    {
+        direction = Vector2.zero;
+        touched = false;
+    }
+
+    public void OnPointerDown (PointerEventData data)
+    {
+        if (!touched)
+        {
+            touched = true;
+            pointerID = data.pointerId;
+            origin = data.position;
+        }
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        Vector2 currentPosition = data.position;
+        Vector2 directionRaw = currentPosition - origin;
+        Vector2 direction = directionRaw.normalized;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        if(data.pointerId == pointerID)
+        {
+            direction = Vector3.zero;
+            touched = false;
+        }
+    }
+
+    public Vector2 GetDirection()
+    {
+        smoothDirection = Vector2.MoveTowards(smoothDirection, direction, smoothing);
+        return smoothDirection;
+    }
+}
